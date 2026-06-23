ea85c2df6b3b0c6e9a4f0526a6e089c202078760
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-06-23 13:24:02 -0400

    refactor: move Square to separate file for my sanity

--- backend/chess-lib/src/lib.rs
+++ backend/chess-lib/src/lib.rs
@@ -1,13 +1,16 @@
 #![allow(dead_code)]
 
 mod moves;
+pub mod square;
 
 use std::{
     fmt::Display,
     ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign},
 };
 
-use strum::{EnumCount, EnumIter, IntoEnumIterator};
+use strum::{EnumCount, IntoEnumIterator};
+
+use crate::square::Square;
 
 pub struct Chessboard {
     pieces: [[Bitboard; PieceType::COUNT]; Side::COUNT],
@@ -76,108 +79,6 @@ impl Display for Chessboard {
         write!(f, "{}", board)?;
 
         Ok(())
-    }
-}
-
-#[derive(EnumCount, EnumIter, Clone, Copy)]
-#[repr(u8)]
-pub enum Square {
-    A1 = 0,
-    B1,
-    C1,
-    D1,
-    E1,
-    F1,
-    G1,
-    H1,
-    A2,
-    B2,
-    C2,
-    D2,
-    E2,
-    F2,
-    G2,
-    H2,
-    A3,
-    B3,
-    C3,
-    D3,
-    E3,
-    F3,
-    G3,
-    H3,
-    A4,
-    B4,
-    C4,
-    D4,
-    E4,
-    F4,
-    G4,
-    H4,
-    A5,
-    B5,
-    C5,
-    D5,
-    E5,
-    F5,
-    G5,
-    H5,
-    A6,
-    B6,
-    C6,
-    D6,
-    E6,
-    F6,
-    G6,
-    H6,
-    A7,
-    B7,
-    C7,
-    D7,
-    E7,
-    F7,
-    G7,
-    H7,
-    A8,
-    B8,
-    C8,
-    D8,
-    E8,
-    F8,
-    G8,
-    H8,
-}
-
-impl Square {
-    /// Generate a bit mask where only the bit corresponding to the value of
-    /// the [`Square`] is set.
-    ///
-    /// For example:
-    ///
-    /// ```rust
-    /// # use chess_lib::Square;
-    /// let square = Square::E4;
-    /// let mask = square.mask();
-    ///
-    /// assert_eq!(mask, 1u64 << 28);
-    /// ```
-    ///
-    /// Visually, this mask would look like this:
-    ///
-    /// ```plaintext
-    /// 00000000
-    /// 00000000
-    /// 00000000
-    /// 00000000
-    /// 00001000
-    /// 00000000
-    /// 00000000
-    /// 00000000
-    /// ```
-    ///
-    /// Where E4 is the only bit set.
-    pub const fn mask(self) -> u64 {
-        1u64 << (self as u8)
     }
 }
 
